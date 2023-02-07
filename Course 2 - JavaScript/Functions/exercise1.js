16e80d8b288908c4d68be0704647df7dd22dd43f
Eric-Obeng <obengeric.3958@gmail.com>
2023-02-07 12:53:50 +0000

    Programming with JavaScript

---
+++ Course 2 - JavaScript/Functions/exercise1.js
@@ -0,0 +1,15 @@
+//Practicing with functions
+//Your task in this exercise is to code a function which will be able to take a word and locate the position of a chosen letter in that given word.
+
+function letterFinder(word, match) {
+    for (var i=0; i < word.length; i++) {
+        if (word[i] == match) {
+            console.log('Found the', match, 'at', i)
+        } 
+        else {
+            console.log('---No match found at', i)
+        }
+    }
+}
+
+letterFinder('javascript', 'a');
