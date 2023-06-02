de7ab94965c0ca78d7b9970cd800558643b2abd0
Eric-Obeng <obengeric.3958@gmail.com>
2023-06-02 23:07:19 +0000

    Meta FrontEnd Dev

--- Course 2 - JavaScript/Object Oriented Programming/DataStructures/map.js
+++
@@ -1,5 +0,0 @@
-[0, 10, 20, 30, 40, 50].map(function(num) {
-    return num/10;
-})
-
-//This retuns [0, 1, 2, 3, 4, 5]
--- Course 2 - JavaScript/Object Oriented Programming/DataStructures/workingWithSet.js
+++
@@ -1,6 +0,0 @@
-//To build a new set, you can use the Set constructor:
-//we use new Set()
-
-const repetitiveFruits = ['apple','pear','apple','pear','plum', 'apple'];
-const uniqueFruits = new Set(repetitiveFruits);
-console.log(uniqueFruits);
